b1e40aa20bf2d8effa9dd891d96d36c2ffe73ca1
Trevor Good <tdgood@users.noreply.github.com>
2024-04-12 22:44:28 -0400

    First push. Basics.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-# Hello world
+import pygame
+
+from pygame.locals import QUIT
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    screen.fill((200, 200, 200))
+
+    pygame.display.update()
+    clock.tick(30)
+
+pygame.quit()
