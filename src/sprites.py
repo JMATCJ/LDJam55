84f0c442444af0cd3129dff8bf9b796eace0b776
Parker Young <parker@parker8283.net>
2024-04-13 20:19:52 -0500

    Basic walking and attacking animations

--- src/sprites.py
+++ src/sprites.py
@@ -1,6 +1,10 @@
 import enum
 import pygame
+from pygame import image, Surface
+from pygame.font import Font
 from pygame.math import Vector2
+from pygame.sprite import Group, Sprite
+from pygame.transform import smoothscale
 
 from consts import *
 
@@ -26,7 +30,7 @@ class Class(enum.Enum):
                 return Zombie(centerpos, *groups)
 
 
-class Unit(pygame.sprite.Sprite):
+class Unit(Sprite):
     def __init__(self, centerpos: tuple[int, int], image_folder_name: str, health: int, attack: int, speed: int, *groups):
         super().__init__(*groups)
 
@@ -36,11 +40,15 @@ class Unit(pygame.sprite.Sprite):
         self.speed = speed
         self.time_since_last_attack = 0
 
+        # For animations
+        self.walking = False
+        self.animation_timer = 0
+
         # Surfaces
-        self.standing_surf = pygame.transform.smoothscale(pygame.image.load(ASSETS_DIR / image_folder_name / "standing.png").convert_alpha(), (64, 64))
-        self.attacking_surf = pygame.transform.smoothscale(pygame.image.load(ASSETS_DIR / image_folder_name / "attacking.png").convert_alpha(), (64, 64))
-        self.walking_1_surf = pygame.transform.smoothscale(pygame.image.load(ASSETS_DIR / image_folder_name / "walking_1.png").convert_alpha(),(64, 64))
-        self.walking_2_surf = pygame.transform.smoothscale(pygame.image.load(ASSETS_DIR / image_folder_name / "walking_2.png").convert_alpha(), (64, 64))
+        self.standing_surf = smoothscale(image.load(ASSETS_DIR / image_folder_name / "standing.png").convert_alpha(), (64, 64))
+        self.attacking_surf = smoothscale(image.load(ASSETS_DIR / image_folder_name / "attacking.png").convert_alpha(), (64, 64))
+        self.walking_1_surf = smoothscale(image.load(ASSETS_DIR / image_folder_name / "walking_1.png").convert_alpha(),(64, 64))
+        self.walking_2_surf = smoothscale(image.load(ASSETS_DIR / image_folder_name / "walking_2.png").convert_alpha(), (64, 64))
 
         # The surface that should be currently drawn
         self.surf = self.standing_surf
@@ -48,12 +56,28 @@ class Unit(pygame.sprite.Sprite):
 
     def update(self, screen_rect, group, delta_time):
         self.time_since_last_attack += delta_time
+        self.animation_timer += delta_time
+        if self.walking:
+            if self.surf not in [self.walking_1_surf, self.walking_2_surf]:
+                self.animation_timer = 0
+                self.surf = self.walking_1_surf
+            elif self.animation_timer >= 400:
+                if self.surf == self.walking_1_surf:
+                    self.surf = self.walking_2_surf
+                else:
+                    self.surf = self.walking_1_surf
+                self.animation_timer = 0
+        elif self.surf == self.attacking_surf:
+            if self.animation_timer >= 150:
+                self.surf = self.standing_surf
+        else:
+            self.surf = self.standing_surf
 
     def draw(self, screen):
         screen.blit(self.surf, self.rect)
 
-    def move_nearest_ip(self, group: pygame.sprite.Group):
-        if group:
+    def move_nearest_ip(self, group: Group):
+        if group and self.surf != self.attacking_surf:
             cur_pos = Vector2(self.rect.center)
             nearest = min([e for e in group], key=lambda e: cur_pos.distance_to(Vector2(e.rect.center)))
             nearest_pos = Vector2(nearest.rect.center)
@@ -62,12 +86,17 @@ class Unit(pygame.sprite.Sprite):
                 vec = dist.normalize() * self.speed
                 if not pygame.sprite.spritecollideany(self, group):
                     self.rect.move_ip(vec)
+                    self.walking = True
+                    return
+        self.walking = False
 
-    def attack_one(self, group: pygame.sprite.Group):
+    def attack_one(self, group: Group):
         if self.time_since_last_attack >= 2000:
             attacked: Unit = pygame.sprite.spritecollideany(self, group)
             if attacked:
                 attacked.health -= self.attack
+                self.surf = self.attacking_surf
+                self.animation_timer = 0
                 if attacked.health <= 0:
                     attacked.kill()
                 self.time_since_last_attack = 0
@@ -127,15 +156,15 @@ class Zombie(Unit):
         self.attack_one(all_players)
 
 
-class TextArea(pygame.sprite.Sprite):
+class TextArea(Sprite):
     # Besides font and value, takes a third parameter for its position, which must be keyworded.
     # Either specify "topleft" or "center", followed by the tuple position.
     # Examples: TextArea(font, "test value", group, topleft=(10, 10))
     #           TextArea(font, "new value", group, center=(20, 30))
-    def __init__(self, font: pygame.font.Font, value: str, *groups, **pos):
+    def __init__(self, font: Font, value: str, *groups, **pos):
         super().__init__(*groups)
         self.font = font
-        self.text: pygame.Surface | None = None
+        self.text: Surface | None = None
         self.set_text(value)
         if "topleft" in pos:
             self.rect = self.text.get_rect(topleft=pos["topleft"])
@@ -152,7 +181,7 @@ class TextArea(pygame.sprite.Sprite):
 
 
 class PlayableUnitsText(TextArea):
-    def __init__(self, font: pygame.font.Font, class_type: Class, *groups, **pos):
+    def __init__(self, font: Font, class_type: Class, *groups, **pos):
         super().__init__(
             font, f"{class_type.name.capitalize()} units: 0", *groups, **pos
         )
@@ -164,12 +193,12 @@ class PlayableUnitsText(TextArea):
         )
 
 
-class TitleScreenArrow(pygame.sprite.Sprite):
+class TitleScreenArrow(Sprite):
     def __init__(self, arrow_type, class_type, *groups, **pos):
         super().__init__(*groups)
         self.arrow_type = arrow_type
         self.class_type = class_type
-        self.surf = pygame.Surface((15, 15))
+        self.surf = Surface((15, 15))
         self.surf.fill((0, 0, 0))
         if "topleft" in pos:
             self.rect = self.surf.get_rect(topleft=pos["topleft"])
@@ -190,7 +219,7 @@ class TitleScreenArrow(pygame.sprite.Sprite):
 
 
 class TitleScreenPlayableUnitsText(TextArea):
-    def __init__(self, font: pygame.font.Font, class_type, *groups, **pos):
+    def __init__(self, font: Font, class_type, *groups, **pos):
         super().__init__(font, "0", *groups, **pos)
         self.class_type = class_type
 
@@ -198,13 +227,13 @@ class TitleScreenPlayableUnitsText(TextArea):
         self.set_text(f"{game.playable_units[self.class_type]}")
 
 
-class Button(pygame.sprite.Sprite):
+class Button(Sprite):
     # Can't type the handle_click_func because of circular imports.
     # It should be a function that takes a gamestate, any returned values are ignored.
     def __init__(self, color: tuple[int, int, int], handle_click_func, *groups, **pos):
         super().__init__(*groups)
 
-        self.surf = pygame.Surface((50, 25))
+        self.surf = Surface((50, 25))
         self.surf.fill(color)
         self.rect = self.surf.get_rect(**pos)
         self.click_func = handle_click_func
